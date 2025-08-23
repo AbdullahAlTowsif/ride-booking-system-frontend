78f929e39399a3cada885167289c04887be5ce25
AbdullahAlTowsif <abdullah.al.towsif2002@gmail.com>
2025-08-24 00:53:33 +0600

    block user restriction added to forntend

--- src/utils/WithAuth.tsx
+++ src/utils/WithAuth.tsx
@@ -6,9 +6,14 @@ import { Navigate } from "react-router";
 export const withAuth = (Component: ComponentType, requiredRole?: TRole) => {
     return function AuthWrapper () {
         const { data, isLoading } = useUserInfoQuery(undefined);
+        console.log("withauth", data);
 
         if(!isLoading && !data?.data?.email) {
             return <Navigate to="/login" />
+        }
+
+        if(!isLoading && data?.data?.isBlock === "BLOCK") {
+            return <Navigate to="/" />
         }
 
         if(requiredRole && !isLoading && requiredRole !== data?.data?.role) {
